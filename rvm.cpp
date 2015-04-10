9b0596cb1c6be7da227a2717e28bd559d811c4a0
Flavio Castro <flaviojr123@gmail.com>
2015-04-10 17:27:22 -0400

    modified:   rvm.cpp 	modified:   rvm_internal.h

--- rvm.cpp
+++ rvm.cpp
@@ -1,6 +1,14 @@
 #include "rvm.h"
+#include <stdlib.h>
+#include <string>
+#include <stdio.h>
+using namespace std
 
 rvm_t rvm_init(const char *directory) {
+  string dir = string(directory);
+  string cmd="mkdir "+dir;
+  system(cmd);
+  return directory;
 }
 
 void *rvm_map(rvm_t rvm, const char *segname, int size_to_create) {
@@ -10,9 +18,23 @@ void rvm_unmap(rvm_t rvm, void *segbase) {
 }
 
 void rvm_destroy(rvm_t rvm, const char *segname) {
+  string filename = string(rvm)+"/"+string(segname);
+  string touch= "touch "+filename;
+  string cmd = "rm "+filename;
+  system(touch);
+  system(cmd);
+  //If system cmd runs before system touch returns we might have a problem      
 }
 
 trans_t rvm_begin_trans(rvm_t rvm, int numsegs, void **segbases) {
+  trans_t new;
+  //If segments are not in use then create a trans_t 
+  //this is not the best way
+  new.rvm=rvm;
+  new.numsegs=numsegs;
+  new.segbases=segbases;
+  //update list of segments
+  return trans_t;
 }
 
 void rvm_about_to_modify(trans_t tid, void *segbase, int offset, int size) {
--- rvm_internal.h
+++ rvm_internal.h
@@ -1,5 +1,12 @@
-struct rvm_t {
+/*struct rvm_t {
 };
+Why should rvm_t be a struct?
+*/
+typedef const char* rvm_t;
 
 struct trans_t {
+  //int tid;
+  rvm_t rvm;
+  int numsegs;
+  void** segbase;
 };
