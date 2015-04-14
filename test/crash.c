4eb38a22bf81738cef5c3c24967149198d6e355b
Flavio Castro <castro.flavio@gatech.edu>
2015-04-14 09:49:36 -0700

    Crash test_case written, needs test

--- test/crash.c
+++ test/crash.c
@@ -9,6 +9,8 @@
 #include <signal.h>
 #include "rvm.h"
 
+static int *glob_var;
+
 char * makeString(int max){
    int i;
    char *test_string = (char*) malloc( sizeof (char) * 1000000);
@@ -33,67 +35,79 @@ int lastInt(char *string){
 /* proc1 writes some data, commits it, then exits */
 void proc1(int n_write)
 {
-   char* segs[1];
-   trans_t trans;
-   rvm_t rvm;
-   char *string=makeString(n_write);
-   //printf("%s\n",string);
-   int n = lastInt(string);
-   printf("%d\n",n);
-   /*rvm = rvm_init("rvm_segments");
-   rvm_destroy(rvm, "int_string");
-   segs[0] = (char *) rvm_map(rvm, "int_string", 1000000);
 
-   trans = rvm_begin_trans(rvm, 1, (void **) segs);
-   rvm_about_to_modify(trans, segs[0], 0, 1000000);
-   sprintf(segs[0], test_string);
-   rvm_commit_trans(trans);
-   */
-   abort();
+     rvm_t rvm;
+     trans_t trans;
+     char* segs[1];
+
+     rvm = rvm_init("rvm_segments");
+     rvm_destroy(rvm, "int_string");
+
+     segs[0] = (char *) rvm_map(rvm, "int_string", 1000000);
+
+     trans = rvm_begin_trans(rvm, 1, (void **) segs);
+
+     rvm_about_to_modify(trans, segs[0], 0, 10);
+     sprintf(segs[0], "0");
+     rvm_commit_trans(trans);
+
+     trans = rvm_begin_trans(rvm, 1, (void **) segs);
+     rvm_about_to_modify(trans, segs[0], 0, 1000000);
+     sprintf(segs[0], makeString(n_write));
+
+     //make this atomic?
+     rvm_commit_trans(trans);
+     *glob_var=n_write-1;
+     //make this atomic?
+     abort();
+
 }
 
 /* proc2 opens the segments and reads from them */
 void proc2(int n_write)
 {
-
     char* segs[1];
     rvm_t rvm;
-    /*rvm = rvm_init("rvm_segments");
+    rvm = rvm_init("rvm_segments");
     segs[0] = (char *) rvm_map(rvm, "int_string", 1000000);
-    char* token=strtok(segs[0], " ");
-    char* last;
-    while (token){last=token;
-                  token=strtok(NULL," ");}
-    int n=atoi(last);
-    if (n == (n_write-1)){
-        printf("OK");
+    int n = lastInt(segs[0]);
+    if (glob_var == n){
+       printf("OK");
     }else{
-        printf("error:%d",n);}
-     
-     exit(0);*/
+       if (glob_var==1){
+         //we crashed
+         if (n==0){
+           //we recovered from crash
+           printf("crash: OK");
+	 }else{
+           //we actually crashed
+           printf("error:%d",n);
+         }
+       }
+    }
+    exit(0);
 }
 
 
 int main(int argc, char **argv)
 {
-     if(argc!=2){printf( "usage: %s n_writes\n", argv[0]);
+     if(argc!=2){printf( "usage: %s max_int\n", argv[0]);
          return -1;}
      int pid;
      int n = atoi(argv[1]);
-
+     glob_var = mmap(NULL, sizeof *glob_var, PROT_READ | PROT_WRITE, 
+                    MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+     *glob_var=1;
      pid = fork();
-     if(pid < 0) {
-	  perror("fork");
-	  exit(2);
-     }
-     if(pid == 0) {
-	  proc1(n);
-	  exit(0);
-     }
-     //sleep(2);
-     //kill(pid,SIGKILL);
+     if(pid < 0) {perror("fork");
+	          exit(2);}
+     if(pid == 0){proc1(n);
+	          exit(0);}
+     //waitpid(pid,NULL,0);
+     sleep(1);
+     kill(pid,SIGKILL);
 
-     //proc2(n);
+     proc2(n);
 
      return 0;
 }
